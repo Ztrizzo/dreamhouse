f3dfe3d7c3ee7d7e5d30eb7794586f2a77892021
Zach Rizzo <rizzozachary2@gmail.com>
2023-07-04 20:43:38 -0500

    added log

--- force-app/main/default/lwc/example/example.js
+++ force-app/main/default/lwc/example/example.js
@@ -1,3 +1,8 @@
 import { LightningElement } from 'lwc';
 
-export default class Example extends LightningElement {}
+export default class Example extends LightningElement {
+
+  connectedCallback(){
+    console.log('test');
+  }
+}
